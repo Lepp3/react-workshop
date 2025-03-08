44948362de4f02eb09172da871d9a92601812803
lmilanov <lubaka05@gmail.com>
2025-03-08 12:17:49 +0200

    populate fields on edit user modal

--- client/src/components/CreateUser.jsx
+++ client/src/components/CreateUser.jsx
@@ -1,10 +1,24 @@
+import { useEffect, useState } from "react"
+import userService from "../services/userService.js";
+
 export default function CreateUser({
     userId,
     onClose,
     onSave,
     onEdit
 }){
-   
+    const [user,setUser] = useState({})
+
+   useEffect(()=>{
+    if(!userId){
+      return;
+    }
+    userService.getOne(userId)
+    .then(result=>{
+      setUser(result);
+    })
+   },[userId])
+
     return(
         <div className="overlay">
       <div className="backdrop" onClick={onClose}></div>
@@ -27,14 +41,14 @@ export default function CreateUser({
                 <label htmlFor="firstName">First name</label>
                 <div className="input-wrapper">
                   <span><i className="fa-solid fa-user"></i></span>
-                  <input id="firstName" name="firstName" type="text" />
+                  <input id="firstName" name="firstName" type="text" defaultValue={user.firstName}/>
                 </div>
               </div>
               <div className="form-group">
                 <label htmlFor="lastName">Last name</label>
                 <div className="input-wrapper">
                   <span><i className="fa-solid fa-user"></i></span>
-                  <input id="lastName" name="lastName" type="text" />
+                  <input id="lastName" name="lastName" type="text" defaultValue={user.lastName}/>
                 </div>
               </div>
             </div>
@@ -44,14 +58,14 @@ export default function CreateUser({
                 <label htmlFor="email">Email</label>
                 <div className="input-wrapper">
                   <span><i className="fa-solid fa-envelope"></i></span>
-                  <input id="email" name="email" type="text" />
+                  <input id="email" name="email" type="text" defaultValue={user.email}/>
                 </div>
               </div>
               <div className="form-group">
                 <label htmlFor="phoneNumber">Phone number</label>
                 <div className="input-wrapper">
                   <span><i className="fa-solid fa-phone"></i></span>
-                  <input id="phoneNumber" name="phoneNumber" type="text" />
+                  <input id="phoneNumber" name="phoneNumber" type="text" defaultValue={user.phoneNumber}/>
                 </div>
               </div>
             </div>
@@ -60,7 +74,7 @@ export default function CreateUser({
               <label htmlFor="imageUrl">Image Url</label>
               <div className="input-wrapper">
                 <span><i className="fa-solid fa-image"></i></span>
-                <input id="imageUrl" name="imageUrl" type="text" />
+                <input id="imageUrl" name="imageUrl" type="text" defaultValue={user.imageUrl}/>
               </div>
             </div>
 
@@ -69,14 +83,14 @@ export default function CreateUser({
                 <label htmlFor="country">Country</label>
                 <div className="input-wrapper">
                   <span><i className="fa-solid fa-map"></i></span>
-                  <input id="country" name="country" type="text" />
+                  <input id="country" name="country" type="text" defaultValue={user.address?.country}/>
                 </div>
               </div>
               <div className="form-group">
                 <label htmlFor="city">City</label>
                 <div className="input-wrapper">
                   <span><i className="fa-solid fa-city"></i></span>
-                  <input id="city" name="city" type="text" />
+                  <input id="city" name="city" type="text" defaultValue={user.address?.city}/>
                 </div>
               </div>
             </div>
@@ -86,14 +100,14 @@ export default function CreateUser({
                 <label htmlFor="street">Street</label>
                 <div className="input-wrapper">
                   <span><i className="fa-solid fa-map"></i></span>
-                  <input id="street" name="street" type="text" />
+                  <input id="street" name="street" type="text" defaultValue={user.address?.street}/>
                 </div>
               </div>
               <div className="form-group">
                 <label htmlFor="streetNumber">Street number</label>
                 <div className="input-wrapper">
                   <span><i className="fa-solid fa-house-chimney"></i></span>
-                  <input id="streetNumber" name="streetNumber" type="text" />
+                  <input id="streetNumber" name="streetNumber" type="text" defaultValue={user.address?.streetNumber}/>
                 </div>
               </div>
             </div>
